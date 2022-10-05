24bef7eedcef8641db985dcd025c836173715d72
Leonardo Alves da Silva <leoal_ves@hotmail.com>
2022-10-05 16:09:05 -0300

    Correção do cadastro usuários
    Correção das telas de cadastrar usuários.

--- view/cadastrarUsuario.php
+++ view/cadastrarUsuario.php
@@ -1,23 +1,25 @@
 <section>
     <div class="limitesUsuarios">
         <article style="left: 50%; position: relative; margin-left: -350px;">
-            <fieldset id="usuario" class="boxCadastro">
-                <p class="boxLoginLabel">Novo Usuario</p>
-                <div class="faixaBrancaBoxLogin"></div>
-                <label class="boxLoginLabel" for="cNome">Nome:</label>
-                <input class="boxLoginInput" type="text" name="nome" id="cNome" size="20" maxlength="30" placeholder="Nome Completo">
-                <label class="boxLoginLabel" for="cSobrenome">Sobrenome:</label>
-                <input class="boxLoginInput" type="text" name="nome" id="cNome" size="20" maxlength="30" placeholder="Nome Completo">
-                <label class="boxLoginLabel" for="cEmail">Email: </label>
-                <input class="boxLoginInput" type="text" name="nome" id="cNome" size="20" maxlength="30" placeholder="Nome Completo">
-                <label class="boxLoginLabel" for="cSenha">Senha: </label>
-                <input class="boxLoginInput" type="text" name="nome" id="cNome" size="20" maxlength="30" placeholder="Nome Completo">
-                <label class="boxLoginLabel" for="cTelefone"> Telefone: </label>
-                <input class="boxLoginInput" type="text" name="nome" id="cNome" size="20" maxlength="30" placeholder="Nome Completo">
-                <label class="boxLoginLabel" for="cCargo"> Cargo: </label>
-                <input class="boxLoginInput" type="text" name="nome" id="cNome" size="20" maxlength="30" placeholder="Nome Completo">
-                <input class="botao" type="submit">
-            </fieldset>
+            <form method="POST" action="/UTFPR/Projeto/projeto22/model/cadastrarUsuario.php">
+                <fieldset id="usuario" class="boxCadastro">
+                    <p class="boxLoginLabel">Novo Usuario</p>
+                    <div class="faixaBrancaBoxLogin"></div>
+                    <label class="boxLoginLabel" for="cNome">Nome:</label>
+                    <input class="boxLoginInput" type="text" name="cNome" id="cNome" size="20" maxlength="30" placeholder="Nome">
+                    <label class="boxLoginLabel" for="cSobrenome">Sobrenome:</label>
+                    <input class="boxLoginInput" type="text" name="cSobrenome" id="cSobrenome" size="20" maxlength="30" placeholder="Sobrenome">
+                    <label class="boxLoginLabel" for="cEmail">Email: </label>
+                    <input class="boxLoginInput" type="text" name="cEmail" id="cEmail" size="20" maxlength="30" placeholder="Email">
+                    <label class="boxLoginLabel" for="cSenha">Senha: </label>
+                    <input class="boxLoginInput" type="password" name="cSenha" id="cSenha" size="20" maxlength="30" placeholder="Senha">
+                    <label class="boxLoginLabel" for="cTelefone"> Telefone: </label>
+                    <input class="boxLoginInput" type="text" name="cTelefone" id="cTelefone" size="20" maxlength="30" placeholder="Telefone">
+                    <label class="boxLoginLabel" for="cCargo"> Cargo: </label>
+                    <input class="boxLoginInput" type="text" name="cCargo" id="cCargo" size="20" maxlength="30" placeholder="Cargo">
+                    <input class="botao" type="submit">
+                </fieldset>
+            </form>
 
         </article>
     </div>
